ee97a7c34e94d0ce65972a7fcef9dc42c7911785
Timele Msibi <timfungulo@gmail.com>
2025-01-02 21:41:38 +0500

    21

--- vc1.c
+++ vc1.c
@@ -4,17 +4,17 @@
 int main() {
 
     // Calculation algorithm
-    int x1, y1, x2, y2;
+    int x11, y1, x2, y2;
 
     printf("Enter the coordinates of the first point (x1, y1): ");
-    scanf("%d %d", &x1, &y1);
+    scanf("%d %d", &x11, &y1);
 
     printf("Enter the coordinates of the second point (x2, y2): ");
     scanf("%d %d", &x2, &y2);
 
-    double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    double distance = sqrt(pow(x2 - x11, 2) + pow(y2 - y1, 2));
 
-    printf("Computed distance between two points on the plane (%d, %d) and (%d, %d): %.2f\n", x1, y1, x2, y2, distance);
+    printf("Computed distance between two points on the plane (%d, %d) and (%d, %d): %.2f\n", x11, y1, x2, y2, distance);
 
     return 0;
 }
